cc553e2e8f6f26610bc4e5bd51b708e52ffd033a
Sagar Shirgaonkar <sagarshirgaonkar59511@gmail.com>
2024-09-12 21:20:43 +0530

    first commit

---
+++ Backend/index.js
@@ -0,0 +1,23 @@
+const connectMongo = require("./db");
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+  res.status(200).json({"message":"success"});
+})
+
+//Routes
+app.use("/api/auth",require("./routes/auth"));
+app.use("/api/notes",require("./routes/notes"));
+
+app.listen(port,()=>{
+  console.log(`NoteBook server running on port ${port}`);
+})
+
+connectMongo();
